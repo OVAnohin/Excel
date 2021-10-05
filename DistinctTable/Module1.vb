708463b02981257304b0f2b21b5d42998595fb44
Oleg Anohin <anohin_o@mail.ru>
2021-10-05 14:29:43 +0400

    1

--- DistinctTable/Module1.vb
+++ DistinctTable/Module1.vb
@@ -1,37 +1,48 @@
-﻿Module Module1
+﻿Imports System.Data.OleDb
+
+Module Module1
 
     Sub Main()
-        Dim dataTable As DataTable = New DataTable("test")
-        dataTable.Columns.Add("Name", Type.GetType("System.String"))
-        dataTable.Columns.Add("ClassName", Type.GetType("System.String"))
-        dataTable.Columns.Add("NativeWindowHandle", Type.GetType("System.Int32"))
+        Dim oConnection As OleDbConnection
+        Dim connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=d:\Time\Work3.xlsx;" + "Extended Properties='Excel 12.0 Xml;HDR=YES;'"
+        If oConnection Is Nothing Then
+            oConnection = New OleDbConnection(connectionString)
+            oConnection.Open()
+        End If
 
-        For i As Integer = 0 To 10
-            Dim newRow As DataRow = dataTable.NewRow()
-            newRow("Name") = "Name " & i
-            newRow("ClassName") = "ClassName " & i
-            newRow("NativeWindowHandle") = i
-            dataTable.Rows.Add(newRow)
-        Next
-        For i As Integer = 0 To 10
-            If i = 2 Or i = 3 Or i = 6 Or i = 8 Then
-                Continue For
-            End If
-            Dim newRow As DataRow = dataTable.NewRow()
-            newRow("Name") = "Name " & i
-            newRow("ClassName") = "ClassName " & i
-            newRow("NativeWindowHandle") = i
-            dataTable.Rows.Add(newRow)
-        Next
-
-        ShowTable(dataTable)
+        Dim sheetName As String = "Sheet1"
+        'sheetName = "Sheet1"
+        Dim oDataAdapter As New OleDbDataAdapter("Select * from[" & sheetName & "$]", oConnection)
+        Dim oDataSet As New DataSet
+        Dim dataTable As DataTable
+        oDataAdapter.Fill(oDataSet)
+        dataTable = oDataSet.Tables(0)
+        oConnection.Close()
+        oConnection = Nothing
 
         Dim view As New DataView(dataTable)
-        view.Sort = "ClassName"
-        'PrintTableOrView(view, "ClassName")
-        'ShowTable(view)
-        Dim newTable As DataTable = view.ToTable(True)
-        ShowTable(newTable)
+        view.Sort = "Номер Договора, Дата DESC, Время DESC"
+        Dim newTable As DataTable = view.ToTable()
+
+        Console.WriteLine()
+
+        For i As Integer = 0 To newTable.Rows.Count - 2
+            Dim currentRow As DataRow = newTable.Rows(i)
+            Dim rowPlusOne As DataRow = newTable.Rows(i + 1)
+            If currentRow(newTable.Columns("Номер Договора")) = rowPlusOne(newTable.Columns("Номер Договора")) Then
+                rowPlusOne(newTable.Columns("Новое значение")) = currentRow(newTable.Columns("Новое значение"))
+                rowPlusOne(newTable.Columns("Старое значение")) = currentRow(newTable.Columns("Старое значение"))
+                rowPlusOne(newTable.Columns("Время")) = currentRow(newTable.Columns("Время"))
+            End If
+        Next
+
+        view = New DataView(newTable)
+        Dim result As DataTable
+        result = view.ToTable(True)
+
+        ShowTable(result)
+
+        Console.ReadKey()
     End Sub
 
     Private Sub ShowTable(table As DataTable)
